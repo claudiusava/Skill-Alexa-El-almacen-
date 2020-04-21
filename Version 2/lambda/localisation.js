8950910c3229f9a9c14c4bebfb56b41a1d3cfb79
Claudiu Sava <47026643+claudiusava@users.noreply.github.com>
2020-04-21 19:59:44 +0200

    Add files via upload

---
+++ Version 2/lambda/localisation.js
@@ -0,0 +1,15 @@
+module.exports = {
+    es:{
+        translation: {
+      MSG_BIENVENIDA:  'Hola! Puedes guardar, listar, buscar o eliminar. ¿Qué deseas hacer?',
+      MSG_PRODUCTO_REGISTRADO: 'Hecho! ¿Quiéres hacer algo más?',
+      MSG_PRODUCTOS_REGISTRADOS: 'Recordaré que has guardado %s %s.',
+      MSG_LISTAR_PRODUCTOS: 'Tienes guardado %s %s',
+      MSG_ALGO_MAS: '¿Quiéres hacer algo más?',
+      MSG_AYUDA: 'Esta es una skill que puede memorizar los productos de tu almacen. Puedes probar a guardar un producto, listar todos los que tienes, buscar por nombre para recordarte los que quedan o eliminarlos. Prueba a decirme lo que deseas.',
+      MSG_FALLBACK: 'Lo siento, no se nada sobre eso. Por favor inténtalo otra vez.',
+      MSG_ERROR: 'Lo siento, ha habido un problema. Por favor inténtalo otra vez.',
+      MSG_DESPEDIDA: 'Hasta luego!'
+    }
+  }
+}
